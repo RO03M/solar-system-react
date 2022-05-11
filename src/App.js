bcc5c37c677fe59e1e87856145059c853277dbd3
Romaeus <romera.gv.s@gmail.com>
2022-05-10 21:39:17 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,33 +1,66 @@
-import { useState } from "react";
-import { motion } from "framer-motion";
+import { useRef, useState } from "react";
+import { motion, useDragControls, useMotionValue, useTransform } from "framer-motion";
 
 import './App.css';
 import SolarSystem from './components/SolarSystem';
+
+const MAX_SCALE = 100;
+const MIN_SCALE = 1;
 
 function App() {
 
 	const [scale, SetScale] = useState(1.0);
 
+	const dragRef = useRef(null);
+
+	const dragControls = useDragControls();
+	const x = useMotionValue(0);
+	const y = useMotionValue(0);
+	const transformX = useTransform(x, value => value * 0.05);
+	const transformY = useTransform(y, value => value * 0.05);
+
+	const bounds = dragRef?.current?.getBoundingClientRect();
+	
 	const handleWheel = e => {
-		let deltaY = e.deltaY * -0.001;
+		let deltaY = e.deltaY * -0.001 * scale / 2;
 		let tempScale = scale + deltaY;
-		if (tempScale < 1) tempScale = 1;
-		else if (tempScale >= 10) tempScale = 10;
+		if (tempScale < MIN_SCALE) tempScale = MIN_SCALE;
+		else if (tempScale >= MAX_SCALE) tempScale = MAX_SCALE;
 		SetScale(tempScale);
 	}
-
+	
 	return (
 		<motion.div
-			drag
+			className={"galaxy"}
+			style={{
+				x: transformX,
+				y: transformY,
+				scale: 1.1
+			}}
 		>
 			<motion.div
-				onWheel={handleWheel}
-				style={{
-					transition: "0.5s",
-					transform: `scale(${scale})`
+				drag
+				dragControls={dragControls}
+				dragConstraints={{
+					left: -bounds?.width / 2.5 * scale,
+					right: bounds?.width / 2.5 * scale,
+					top: -bounds?.height / 5 * scale,
+					bottom: bounds?.height / 5 * scale
 				}}
+				style={{
+					x, y
+				}}
+				ref={dragRef}
 			>
-				<SolarSystem/>	
+				<motion.div
+					onWheel={handleWheel}
+					style={{
+						transition: "0.5s",
+						transform: `scale(${scale})`
+					}}
+				>
+					<SolarSystem/>	
+				</motion.div>
 			</motion.div>
 		</motion.div>
 	);
